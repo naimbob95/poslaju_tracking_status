c03df9f649da661990da5872c2714b80ff5d370d
Amirul Naim <naimbob95@gmail.com>
2021-02-21 04:12:42 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const request = require("request-promise");
+const cheerio = require("cheerio");
+async function get_tracking_status(tracking) {
+    const result = await request.get("https://sendparcel.poslaju.com.my/open/trace?tno="+tracking);
+    const $ = cheerio.load(result);
+    const data = [];
+    $("tr").each((index, element) => {
+        if (index === 0) return true;
+        const tds = $(element).find("td");
+        const date_time = $(tds[0]).text();
+        const status = $(tds[1]).text();
+        const location = $(tds[2]).text();
+        const tableRow = { date_time, status, location };
+        data.push(tableRow);
+    });
+
+    console.log(data);
+    
+}
+
+const tracking = process.argv[2];
+exports.get_tracking_status(tracking);
